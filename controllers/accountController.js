2ed6f01c2e494906eb2d784a14dd8596b76cd14d
Daviles82 <avileshouston@live.com>
2024-03-24 01:27:23 -0500

    comment cleanup

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -13,7 +13,6 @@ const bcrypt = require("bcryptjs")
  * *************************************** */
 async function buildLogin(req, res, next) {
   let nav = await utilities.getNav();
-  // req.flash('notice', 'This is a flash message.');
   res.render('./account/login', {
     title: 'Login',
     nav,
